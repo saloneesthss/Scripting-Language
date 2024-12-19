7a1503b97322d22e15d9c0951fc93f584153b98b
Salonee Shrestha <saloneeshrestha35@gmail.com>
2024-12-19 20:18:59 +0545

    js and php labs

---
+++ php/html_in_php.php
@@ -0,0 +1,17 @@
+<?php
+echo '<h2 style="margin-left:20px;">List of Students</h2>';
+echo '<table border="1"; style="border-collapse:collapse; margin-left:10px;">
+          <tr>
+              <th>Name</th>
+              <th>Email</th>
+          </tr>
+          <tr>
+              <td>Salonee</td>
+              <td>salonee@gmail.com</td>
+          </tr>
+          <tr>
+              <td>Simran</td>
+              <td>simran@gmail.com</td>
+          </tr>
+      </table>';
+?>
---
+++ php/subtract_numbers.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Subtract Numbers</title>
+</head>
+<body>
+    <h2>Difference of Two Numbers</h2>
+    <label for="num1">Enter First Number: </label>
+    <input type="number" id="num1" name="num1"> <br><br>
+    <label for="num2">Enter Second Number: </label>
+    <input type="number" id="num2" name="num2"> <br><br>
+    <label for="sub">Result: </label>
+    <input readonly type="number" id="sub"> <br><br>
+    <button onclick="displayDiff();">Display Difference</button>
+</body>
+</html>
