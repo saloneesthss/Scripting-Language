3a1d9e0b35cef2e87f9446310da9d52f069566ed
Salonee Shrestha <saloneeshrestha35@gmail.com>
2024-12-23 12:29:44 +0545

    objects in js

--- php/subtract_numbers.php
+++ php/subtract_numbers.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,12 +5,14 @@
 </head>
 <body>
     <h2>Difference of Two Numbers</h2>
-    <label for="num1">Enter First Number: </label>
-    <input type="number" id="num1" name="num1"> <br><br>
-    <label for="num2">Enter Second Number: </label>
-    <input type="number" id="num2" name="num2"> <br><br>
-    <label for="sub">Result: </label>
-    <input readonly type="number" id="sub"> <br><br>
-    <button onclick="displayDiff();">Display Difference</button>
+    <form method="post">
+        <label for="num1">Enter First Number: </label>
+        <input type="number" id="num1" name="num1" value="<?php echo isset($_POST['num1']) ? $_POST['num1'] : ''; ?>"> <br><br>
+        <label for="num2">Enter Second Number: </label>
+        <input type="number" id="num2" name="num2" value="<?php echo isset($_POST['num2']) ? $_POST['num2'] : ''; ?>"> <br><br>
+        <label for="sub">Result: </label>
+        <input readonly type="number" id="sub" value="<?php echo isset($_POST['submit']) ? $_POST['num1'] - $_POST['num2'] : ''; ?>"> <br><br>
+        <button type="submit" name="submit" >Subtract</button>
+    </form>
 </body>
 </html>
